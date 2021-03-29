0d8713010d1586167d3cfb13c72d3e673ef32318
rabbit19981023 <rabbit19981023@gmail.com>
2021-03-29 23:40:35 +0800

    updates

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,18 +1,36 @@
 // import the Data Models
-import RoomsListModel from '../models/roomsList.js'
 import RoomsTimeModel from '../models/roomsTime.js'
 
 import isAuth from '../lib/isAuth.js'
 import parsingUser from '../lib/parsingUser.js'
 import timeData from '../lib/timeData.js'
 
+
 /** Routes Controllers **/
 export default {
   index: function (req, res) {
-    return res.render('index', { layout: 'user', isAuth: isAuth(req.user, 'User'), user: user })
+    const user = parsingUser(req)
+
+    return res.render('index', { layout: 'user', user: user })
   },
 
   roomsReserve: function (req, res) {
-    
+    const user = parsingUser(req)
+
+    const data = {
+      rooms: []
+    }
+
+    RoomsTimeModel.getAllRoomsTime(async (error, roomsTime) => {
+      if (error) { return res.render('500error') }
+
+      await roomsTime.forEach((roomTime) => {
+        if (!data.rooms.includes(roomTime.room_name)) {
+          data.rooms.push(roomTime.room_name)
+        }
+      })
+
+      return res.render('roomsReserve', { layout: 'user', isAuth: isAuth(req.user, 'User'), user: user, data: data })
+    })
   }
 }
